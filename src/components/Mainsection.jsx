cce30337d27fc46dcd0bfd962796fa2ccd1f74fb
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-01-29 20:52:47 +0530

    Update metadata and images in layout and Mainsection components

--- src/components/Mainsection.jsx
+++ src/components/Mainsection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Image from 'next/image'
 const Mainsection = () => {
   return (
     <div>
@@ -28,37 +28,28 @@ const Mainsection = () => {
 
       <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
-          <div className="flex w-full mb-20 flex-wrap">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font text-white lg:w-1/3 lg:mb-0 mb-4">
-              Master Cleanse Reliac Heirloom
-            </h1>
-            <p className="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify, subway tile poke farm-to-table. Franzen you probably
-              haven't heard of them man bun deep jianbing selfies heirloom.
-            </p>
-          </div>
+         
           <div className="flex flex-wrap md:-m-2 -m-1">
             <div className="flex flex-wrap w-1/2">
               <div className="md:p-2 p-1 w-1/2">
                 <img
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
-                  src="https://dummyimage.com/500x300"
+                  src="/studyhubscreenshot.jpg"
                 />
               </div>
               <div className="md:p-2 p-1 w-1/2">
                 <img
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
-                  src="https://dummyimage.com/501x301"
+                  src="/studyhubscreenshot.jpg"
                 />
               </div>
               <div className="md:p-2 p-1 w-full">
                 <img
                   alt="gallery"
                   className="w-full h-full object-cover object-center block"
-                  src="https://dummyimage.com/600x360"
+                  src="/pro1.jpg"
                 />
               </div>
             </div>
@@ -67,53 +58,71 @@ const Mainsection = () => {
                 <img
                   alt="gallery"
                   className="w-full h-full object-cover object-center block"
-                  src="https://dummyimage.com/601x361"
+                  src="/studyhubscreenshot.jpg"
                 />
               </div>
               <div className="md:p-2 p-1 w-1/2">
                 <img
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
-                  src="https://dummyimage.com/502x302"
+                  src="/studyhubscreenshot.jpg"
                 />
               </div>
               <div className="md:p-2 p-1 w-1/2">
                 <img
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
-                  src="https://dummyimage.com/503x303"
+                  src="/studyhubscreenshot.jpg"
                 />
               </div>
             </div>
           </div>
         </div>
       </section>
-
       <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
           <img
             className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
             alt="hero"
-            src="https://dummyimage.com/720x600"
+            src="/pro1.jpg"
           />
           <div className="text-center lg:w-2/3 w-full">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-              Microdosing synth tattooed vexillologist
+              StudyHubConnects 
             </h1>
             <p className="leading-relaxed mb-8">
-              Meggings kinfolk echo park stumptown DIY, kale chips beard
-              jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice
-              godard disrupt ramps hexagon mustache umami snackwave tilde
-              chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac
-              mlkshk freegan photo booth af fingerstache pitchfork.
+            
+
             </p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                Button
-              </button>
+             <a href="https://studyhubconnect.vercel.app/" target="_blank"> <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Visit
+              </button></a>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-400 bg-gray-900 body-font">
+        <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+          <img
+            className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+            alt="hero"
+            src="/studyhubscreenshot.jpg"
+          />
+          <div className="text-center lg:w-2/3 w-full">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+              StudyHubConnects Blogs
+            </h1>
+            <p className="leading-relaxed mb-8">
+            Study Hub connects Blogs is a platform where you can read blogs related to various topics. 
+
+            </p>
+            <div className="flex justify-center">
+             <a href="https://blog-somanshubh.vercel.app/" target="_blank"> <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Visit
+              </button></a>
+              
             </div>
           </div>
         </div>
