9f6e725e59b505e52f77e9545ebd82322dcbe84a
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-02-14 19:33:57 +0530

    Remove unnecessary code in Footer and Mainsection components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-       <footer className="text-gray-400 bg-gray-900 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-            
-          </nav>
-          <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
-            
-            <span className="ml-3 text-xl xl:block lg:hidden">Somanshu Bhardwaj</span>
-          </a>
-          <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-            
-          </div>
-        </div>
-      </footer>
+      
       
     </div>
   );
