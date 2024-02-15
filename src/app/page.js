4f90dda1d530f2b50d0f034aa274f6628e5b200e
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-02-15 18:32:43 +0530

    Update Home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
-import Footer from "../components/Footer";
-import Mainsection from "../components/Mainsection";
 import Main from "@/components/Main";
 export default function Home() {
   return (
     <>
       <div className="bg-black text-white">
-        {/*  <Nav />*/}
         <Main />
-        <Mainsection />
-        <Footer />
+        <div className="container mx-auto glass">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-center">Note</h1>
+            <p className="text-center">The website is under development.</p>
+          </div>
+        </div>
       </div>
     </>
   );
